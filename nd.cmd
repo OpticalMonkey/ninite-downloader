95da7dcc85b8cf6577a7c14dc3e462d0e9e565bd
micheal <micheal@espinola.net>
2011-03-06 02:57:48 +0000

    Repository creation

---
+++ nd.cmd
@@ -0,0 +1,108 @@
+@ECHO OFF
+ECHO.
+ECHO [ND] Ninite Downloader 1.0
+ECHO.===============================================================================
+ECHO.
+
+:PREP
+SETLOCAL
+SET TOOLS-BIN=.\bin
+SET NINITELOC=.\Ninite
+SET USERAGENT=GetRight/6.3e
+SET WGET32URL=http://users.ugent.be/~bpuype/cgi-bin/fetch.pl?dl=wget/wget.exe
+
+:CHECKPREREQ
+IF NOT EXIST %TOOLS-BIN%\wget.exe GOTO :PREREQ
+IF NOT EXIST %NINITELOC%\. MD %NINITELOC%
+
+:QUEUE
+ECHO START: %DATE% %TIME%
+ECHO.
+ECHO Downloading Ninite Antimalware Installers:
+CALL :DOWNLOAD adaware
+CALL :DOWNLOAD malwarebytes
+CALL :DOWNLOAD spybot
+CALL :DOWNLOAD super
+ECHO Downloading Ninite Antivirus Installers:
+CALL :DOWNLOAD avast-malwarebytes
+CALL :DOWNLOAD avg-malwarebytes
+CALL :DOWNLOAD essentials-malwarebytes
+ECHO Downloading Ninite Browser Installers:
+CALL :DOWNLOAD chrome
+CALL :DOWNLOAD firefox
+CALL :DOWNLOAD opera
+CALL :DOWNLOAD safari
+ECHO Downloading Ninite File Sharing Installers:
+CALL :DOWNLOAD emule-utorrent
+ECHO Downloading Ninite Web Runtime Installers:
+CALL :DOWNLOAD air-flash-flashie
+CALL :DOWNLOAD dotnet-silverlight
+CALL :DOWNLOAD java
+CALL :DOWNLOAD air-flash-flashie-dotnet-silverlight-java
+ECHO Downloading Ninite Messaging Installers:
+CALL :DOWNLOAD aim
+CALL :DOWNLOAD googletalk
+CALL :DOWNLOAD messenger
+CALL :DOWNLOAD skype
+CALL :DOWNLOAD yahoo
+ECHO Downloading Ninite Multi-Messaging Installers:
+CALL :DOWNLOAD digsby
+CALL :DOWNLOAD pidgin
+ECHO Downloading Ninite Viewer Installers:
+CALL :DOWNLOAD klitecodecs-mediamonkey-picasa-vlc-winamp
+CALL :DOWNLOAD reader
+ECHO Downloading Ninite Archive Installers:
+CALL :DOWNLOAD imgburn-sevenzip-winrar
+ECHO Downloading Ninite Remote Access Installers:
+CALL :DOWNLOAD putty-realvnc-teamviewer
+ECHO Downloading Ninite Remote Transfer Installers:
+CALL :DOWNLOAD filezilla-teracopy-winscp
+ECHO Downloading Ninite Piriform Installers:
+CALL :DOWNLOAD ccleaner-defraggler-recuva
+ECHO.
+ECHO FINISH: %DATE% %TIME%
+GOTO :END
+
+:DOWNLOAD
+ECHO * Downloading %1... & START /WAIT /MIN %TOOLS-BIN%\wget.exe --user-agent=%USERAGENT% --output-document=%NINITELOC%\%1-ninite.exe http://ninite.com/%1/ninite.exe
+GOTO :EOF
+
+:PREREQ
+IF EXIST .\wget.exe (
+	IF NOT EXIST %TOOLS-BIN%\. MD %TOOLS-BIN%
+	MOVE /Y .\wget.exe %TOOLS-BIN%\
+	GOTO :QUEUE
+) ELSE (
+	IF NOT EXIST %TOOLS-BIN%\wget.exe GOTO :PREREQMISSING
+)
+
+:PREREQMISSING
+ECHO ERROR: WGET.EXE is missing.
+ECHO.
+ECHO WGET is essential for aquiring all other files required by this script.
+ECHO The latest Win32 port of WGET can be downloaded from here:
+ECHO.
+ECHO    %WGET32URL%
+ECHO.
+ECHO You must download and save WGET.EXE into the same directory as this script,
+ECHO and try again. For your convenience, the hyperlink above will now be activated.
+ECHO.
+PAUSE
+START %WGET32URL%
+ECHO.
+ECHO Please re-run this script when WGET.EXE has finished downloading, and has been
+ECHO saved or copied into this directory.
+GOTO :END
+
+:END
+ECHO.
+ENDLOCAL
+REM 5678901234567890123456789012345678901234567890123456789012345678901234567890
+REM This script is free to USE and MODIFY, or REDISTRIBUTE in its' ORIGINAL FORM;
+REM as long as this REM'd passage is always included.
+ECHO.
+ECHO.===============================================================================
+ECHO Copyright 2011, Micheal Espinola Jr (micheal@espinola.net)
+ECHO.
+
+:EOF
